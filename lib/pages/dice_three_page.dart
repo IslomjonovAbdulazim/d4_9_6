bc910887b4d4b9b02613e7e0c0fc44335986d137
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-16 18:42:17 +0500

    imp

--- lib/pages/dice_three_page.dart
+++ lib/pages/dice_three_page.dart
@@ -1,3 +1,4 @@
+import 'package:dice_icons/dice_icons.dart';
 import 'package:flutter/material.dart';
 
 class DiceThreePage extends StatefulWidget {
@@ -8,6 +9,26 @@ class DiceThreePage extends StatefulWidget {
 }
 
 class _DiceThreePageState extends State<DiceThreePage> {
+
+
+  IconData getDiceIcon(int number) {
+    if (number == 1) {
+      return DiceIcons.dice1;
+    } else if (number == 2) {
+      return DiceIcons.dice2;
+    } else if (number == 3) {
+      return DiceIcons.dice3;
+    } else if (number == 4) {
+      return DiceIcons.dice4;
+    } else if (number == 5) {
+      return DiceIcons.dice5;
+    } else if (number == 6) {
+      return DiceIcons.dice6;
+    } else {
+      return DiceIcons.dice0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
