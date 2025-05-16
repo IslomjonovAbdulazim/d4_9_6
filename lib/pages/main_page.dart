039eefbe5e3760651acbcceca77aa06966ffc7cc
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-16 17:17:08 +0500

    imp

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -19,3 +21,41 @@ class MainPage extends StatelessWidget {
     );
   }
 }
+
+class _Item extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+  const _Item({super.key, required this.title, required this.page});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      onPressed: () {
+        Get.to(page);
+      },
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
